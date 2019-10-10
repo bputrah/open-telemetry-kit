968cdd07dc166a8a4a41e3ce1857e0c57900e859
Adam <adam@hivemapper.com>
2019-10-10 13:24:23 -0700

    Start implementation of gpxparser

---
+++ open_telemetry_kit/gpxparser.py
@@ -0,0 +1,55 @@
+from .parser import Parser
+from .telemetry import Telemetry
+from .packet import Packet
+from .element import Element, UnknownElement
+
+import xml.etree.ElementTree as ET
+
+class GPXParser(Parser):
+  ext = 'gpx'
+
+  def __init__(self, source):
+    super().__init__(source)
+
+  def read(self):
+    tree = ET.parse(self.source)
+    tel = Telemetry()
+    self._traverse_tree(tree.getroot, tel)
+    return tel
+
+  def _traverse_tree(self, node, tel):
+    for child in node:
+      # Ignore namespace if it exists
+      tag = child.tag[child.tag.find('}')+1:]
+      if tag == "metadata":
+        packet = Packet()
+        self._extract_metadata(child, packet)
+        tel.append(packet)
+      if tag == "trkpt":
+        packet = Packet()
+        self._extract_trkpt(child, packet)
+        tel.append(packet)
+
+      self._traverse_tree(child, tel)
+  
+  def _extract_metadata(self, node, packet):
+    pass
+
+  def _extract_trkpt(self, node, packet):
+    for key, val in node.items:
+      self._add_element(packet, key, val)
+
+    if not node.text.isspace():
+      tag = node.tag[node.tag.find('}')+1:]
+      self._add_element(packet, tag, node.text.strip())
+
+    for child in node:
+      self._extract_metadata(child, packet)
+
+
+  def _add_element(self, packet, key, val):
+      if key in self.element_dict:
+        element_cls = self.element_dict[key]
+        packet[element_cls.name] = element_cls.fromGPX(val)
+      else: 
+        packet[key] = UnknownElement(val)

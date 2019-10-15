47b5d83952acb07cb2b173c24713be0cddab056d
Adam <adam@hivemapper.com>
2019-10-15 14:59:04 -0700

    Add brief description of how the package is designed/organized

--- quickstart/quickstart.py
+++ quickstart/quickstart.py
@@ -8,6 +8,18 @@ import sys
 source = sys.argv[1]
 dest = sys.argv[2]
 
+# This package is organized in Parser, Telemetry, Packet, and Element objects.
+# A Parser is initialized with the source and is used to read in and parse the 
+#    telemetry into a Telemetry object.
+# A Telemetry object is a list of Packet objects.
+# A Packet object is a dictionary of Elements and represents a temporal 
+#    grouping of the telmetry. (e.g. data all recorded during at the same 
+#    timestamp). The key is the canonical name of the data ('latitude', 
+#    'longitude', etc.). The value is the Element object itself 
+# An Element contains the actual value of the reading.
+# There is also a small library of functions used for telemetry type detection
+#    as well as writing the Telemetry to a new file.
+
 # Create Parser object used to parse source file
 parser = detector.create_telemetry_parser(source)
 

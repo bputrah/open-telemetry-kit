5770878f9be314053086dc8ef89e936eba959c68
Adam <adam@hivemapper.com>
2019-10-22 17:36:18 -0700

    Reorder imports

--- open_telemetry_kit/parser.py
+++ open_telemetry_kit/parser.py
@@ -1,7 +1,7 @@
-from abc import ABCMeta
-from abc import abstractmethod
 from .telemetry import Telemetry
 from .element import Element
+from abc import ABCMeta
+from abc import abstractmethod
 
 class Parser(metaclass=ABCMeta):
   def __init__(self, source, require_timestamp: bool = False):

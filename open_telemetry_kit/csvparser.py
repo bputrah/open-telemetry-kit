366d5c3201a26071f8d6536ccbd2ed70da075846
Adam <adam@hivemapper.com>
2019-11-05 16:26:42 -0800

    Fix check for float value

--- open_telemetry_kit/csvparser.py
+++ open_telemetry_kit/csvparser.py
@@ -34,9 +34,9 @@ class CSVParser(Parser):
               val = dup.parse(val).timestamp()
               packet[TimestampElement.name] = TimestampElement(val)
             elif element_cls == TimestampElement:
-              if val.find('.'):
-                # Probably a float in epoch seconds
+              if val.find('.') > 0: #probably a float in epoch seconds
                 packet[element_cls.name] = element_cls(val)
+
               # Based on this SO post: https://bit.ly/2WMGOc1
               elif len(val) >= 16: #probably microseconds
                 packet[element_cls.name] = element_cls(float(val) * 1e-6)

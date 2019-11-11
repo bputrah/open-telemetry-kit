0ada2189f3c91753cbf10cb94bafbb4cf9b6b008
Adam <adam@hivemapper.com>
2019-11-11 13:26:58 -0800

    Add type hints

--- open_telemetry_kit/klv_common.py
+++ open_telemetry_kit/klv_common.py
@@ -1,14 +1,14 @@
-def bytes_to_int(bytes):
-  return int().from_bytes(bytes, byteorder="bigendian", signed=False)
+def bytes_to_int(byte):
+  return int.from_bytes(byte, byteorder="big", signed=False)
 
-def lerp(x, x0, x1, y0, y1):
+def lerp(x: int, x0: int, x1: int, y0: float, y1: float):
   t = (x - x0) / (x1 - x0)
   return (1-t) * y0 + t * y1
 
-def bytes_to_float(bytes, src_min, src_max, dest_min, dest_max):
-  i = int.from_bytes(bytes, byteorder="bigendian", signed=src_min < 0)
+def bytes_to_float(byte, src_min: int, src_max: int, dest_min: float, dest_max: float):
+  i = int.from_bytes(byte, byteorder="big", signed=src_min < 0)
   return lerp(i, src_min, src_max, dest_min, dest_max)
 
 #TODO: This function
-def bytes_to_str(bytes):
-  pass
+def bytes_to_str(byte):
+  return byte.decode("utf-8")

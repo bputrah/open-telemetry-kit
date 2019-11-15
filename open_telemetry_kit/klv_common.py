1eb149e3ca65459baaa8eb435eb763d4a0210d96
Adam <adam@hivemapper.com>
2019-11-14 18:06:08 -0800

    Change function parameters to two tuples

--- open_telemetry_kit/klv_common.py
+++ open_telemetry_kit/klv_common.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 def bytes_to_int(byte):
   return int.from_bytes(byte, byteorder="big", signed=False)
 
@@ -5,9 +7,9 @@ def lerp(x: int, x0: int, x1: int, y0: float, y1: float):
   t = (x - x0) / (x1 - x0)
   return (1-t) * y0 + t * y1
 
-def bytes_to_float(byte, src_min: int, src_max: int, dest_min: float, dest_max: float):
-  i = int.from_bytes(byte, byteorder="big", signed=src_min < 0)
-  return lerp(i, src_min, src_max, dest_min, dest_max)
+def bytes_to_float(byte, src: Tuple[int, int], dest: Tuple[float, float]):
+  i = int.from_bytes(byte, byteorder="big", signed=(src[0] < 0))
+  return lerp(i, src[0], src[1], dest[0], dest[1])
 
 #TODO: This function
 def bytes_to_str(byte):

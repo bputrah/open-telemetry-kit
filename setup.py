6af61b6acbb2f744721c05af78695d93fc2348e6
Adam <adam@hivemapper.com>
2019-11-15 16:47:28 -0800

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="open_telemetry_kit",
-    version="0.2.0a",
+    version="0.2.0b",
     author="Hivemapper",
     author_email="adam@hivemapper.com",
     description="Open source package for extracting and parsing telemetry associated with video streams and converting to common formats.",
